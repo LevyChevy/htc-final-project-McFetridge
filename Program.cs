6732dffc9063c77ae4d25b7ff55b7ef94a469306
User <User@DEV-022776.OnBase.net>
2019-04-16 19:32:48 -0400

    Made stuff

--- Program.cs
+++ Program.cs
@@ -8,13 +8,16 @@ namespace htc_final_project_McFetridge
         {
             Console.WriteLine("Hello kids, welcome to this fortune teller thing. I will predict what career you will presue in the future.");
             Console.WriteLine("");
-            Console.WriteLine("Question 1: Would you rather have an A: easy life or B: adventuris life?");
+            Console.WriteLine("Question 1: Would you rather have an A: easy life or B: adventurous life?");
             string QuestionOne = Console.ReadLine();
-
+            //question 1
             if(QuestionOne == "A"){
-                Console.WriteLine("My man");
-            } 
+                Console.WriteLine("you boring. next question");
 
+            }else if(QuestionOne == "B"){
+                Console.WriteLine("");
+            }
+            //question 2
 
         }
     }
