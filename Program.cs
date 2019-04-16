df110833719f39b913596ab0a51eb3c0bd0713f0
User <User@DEV-022776.OnBase.net>
2019-04-16 18:00:25 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace htc_final_project_McFetridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
