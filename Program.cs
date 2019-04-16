70f50a5b2ca63cda83167e5cf2b1cbf64e0a960a
User <User@DEV-022776.OnBase.net>
2019-04-16 19:28:42 -0400

    Made Things

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace htc_final_project_McFetridge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello kids, welcome to this fortune teller thing. I will predict what career you will presue in the future.");
+            Console.WriteLine("");
+            Console.WriteLine("Question 1: Would you rather have an A: easy life or B: adventuris life?");
+            string QuestionOne = Console.ReadLine();
+
+            if(QuestionOne == "A"){
+                Console.WriteLine("My man");
+            } 
+
+
         }
     }
 }
